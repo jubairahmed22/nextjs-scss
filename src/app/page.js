2bd37c6a9b93b1fa7dfe002f7874628d3a1917f2
jubairahmed22 <jubairahmed060@gmail.com>
2024-03-24 15:20:08 +0600

    sass home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,25 @@
 import Counter from "@/components/Counter/Counter";
-import variables from '../styles/variables.module.scss'
+import variables from "../styles/variables.module.scss";
+import "./sass/_home.scss";
 
 const HomePage = () => {
-
-
   return (
-    <div style={{ background: variables.secondaryColor }}>
+    <div>
       <div style={{ color: variables.primaryColor }}>SCSS</div>
-      <h1 >Welcome time to Next.js home page </h1>
+      <h1 className="textMain">Welcome time to Next.js home page </h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">HTML</a>
+          </li>
+          <li>
+            <a href="#">CSS</a>
+          </li>
+          <li>
+            <a href="#">SASS</a>
+          </li>
+        </ul>
+      </nav>
       <Counter></Counter>
     </div>
   );
