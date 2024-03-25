90255f3836f83577796ebbad689c2a08025a6685
jubairahmed22 <jubairahmed060@gmail.com>
2024-03-25 11:18:42 +0600

    sec

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Counter from "@/components/Counter/Counter";
 import variables from "../styles/variables.module.scss";
 import "./sass/_home.scss";
+import "./sass/mixin.scss"
 
 const HomePage = () => {
   return (
@@ -20,6 +21,11 @@ const HomePage = () => {
           </li>
         </ul>
       </nav>
+      <div className="grid grid-cols-3 gap-5 place-items-center">
+          <div className="w-56 h-56 bg-orange-300 first"></div>
+          <div className="w-56 h-56 bg-orange-300 Sec"></div>
+          <div className="w-56 h-56 bg-orange-300 Thi"></div>
+      </div>
       <Counter></Counter>
     </div>
   );
